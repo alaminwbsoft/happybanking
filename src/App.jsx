0cd4a50ef10053969f8187fc7efc5b2e75ecab38
alaminwbsoft <wbsoft.alamin@gmail.com>
2024-12-06 19:28:29 +0600

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import Login from "./Components/HomePageBeforeLogin/Login";
 
 function App() {
   return (
     <>
+      <Login />
       <h1 className="text-3xl font-bold underline">Hello Al-Amin!</h1>
     </>
   );
